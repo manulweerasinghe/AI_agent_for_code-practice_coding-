283ffebc6aaebfb17f5f1b049a7dabe687967f08
manulweerasinghe <weerasinghemanul@gmail.com>
2026-02-06 09:15:06 +0530

    add role

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
+# CLI argument
 parser = argparse.ArgumentParser(description = "Chatbot")
 parser.add_argument("user_prompt", type = str, help = "User prompt")
 args = parser.parse_args()
 
+#set messages
+messages = [genai.types.Content(role="user", parts=[genai.types.Part(text = args.user_prompt)])]
+
+#get api key
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("The api key not found")
 
+#setup client and send request
 client = genai.Client(api_key = api_key)
 generate_content = client.models.generate_content(
         model = "gemini-2.5-flash",
-        contents = args.user_prompt
+        contents = messages
         )
+
+#count token usage
 usage_metadata = generate_content.usage_metadata
 if usage_metadata == None:
     raise RuntimeError("Failed API request")
